3472657b402a1293086d4ae4ee00e644a6c41506
Satinder Kaur <sandykaur2008@gmail.com>
2018-09-05 18:49:43 -0400

    added more tests

---
+++ test/test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var server = require('../server-dest/server.js'), 
+    expect = require('chai').expect,
+    request = require('request'); 
+
+describe('server response', function () {
+
+  it('should return 404', function(done) {
+    request.get('http://localhost:8080/fake', function (err, res){
+      expect(res.statusCode).to.equal(404);
+      done();
+    }); 
+  }); 
+
+  it('should return 200', function(done) {
+    request.get('http://localhost:8080', function (err, res){
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+}); 
+
+describe('utilities functionality', function () {
+
+  after(function () {
+    server.close();
+   });
+
+  it('should return body of request', function(done) {
+    request.get('http://localhost:8080', function (err, res){
+      expect(res.body).to.equal('Hello World');
+      done();
+    });
+  });
+
+  it('should return headers of request', function(done) {
+    request.get('http://localhost:8080', function (err, res){
+      expect(res.headers).to.include({ 'content-type': 'text/plain' });
+      done();
+    });
+  });
+});
