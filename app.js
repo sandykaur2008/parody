7d56e1255e2f3f06fdd875942390fe789245c353
Satinder Kaur <sandykaur2008@gmail.com>
2018-09-11 20:27:41 -0400

    email works, flashes message

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const app = express(); 
+const bodyParser = require('body-parser'); 
+const mainRouter = require('./src/routes/mainRoutes'); 
+const authRouter = require('./src/routes/authRoutes');  
+const session = require('express-session');
+const flash= require('connect-flash'); 
+const dotenv = require('dotenv');
+
+dotenv.config();   
 
 app.set('views', './src/views'); 
 app.set('view engine', 'ejs'); 
-
-const mainRouter = require('./src/routes/mainRoutes'); 
-const authRouter = require('./src/routes/authRoutes'); 
-
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(bodyParser.json()); 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+})); 
+app.use(flash()); 
 app.use('/', mainRouter); 
+app.use('/auth', authRouter); 
 
 app.listen(4000, function(){
   console.log('listening on port 4000'); 
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router(); 
+const nodemailer = require('nodemailer'); 
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Home'}); 
+  res.render('index', {
+    title: 'Home',
+    messages: req.flash('msg')
+  }); 
+}); 
+
+router.post('/', (req, res) => {
+  let smtpTrans = nodemailer.createTransport({
+    host: process.env.MAIL_SERVER, 
+    port: process.env.MAIL_PORT,
+    secure: true,
+    auth: {
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD
+    }
+  });
+  let mailOpts = {
+    from: req.body.fullname + ' &lt;' + req.body.email + '&gt;',
+    to: process.env.MAIL_USERNAME,
+    subject: 'Parody site message!',
+    text: `${req.body.fullname} (${req.body.email}) says: ${req.body.message}`
+  }; 
+  smtpTrans.sendMail(mailOpts, (error, info) => {
+    if (error) {
+      req.flash('msg', 'Error Occured');
+      res.redirect('/');
+    }
+    req.flash('msg', 'Thanks for your email!'); 
+    res.redirect('/'); 
+  }); 
 }); 
 
 module.exports = router; 
