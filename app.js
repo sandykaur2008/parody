64c0e25b1ce60d5e400e39d2fc85a637e0780337
Satinder Kaur <sandykaur2008@gmail.com>
2018-09-13 18:26:13 -0400

    working on authentication

--- app.js
+++ app.js
@@ -2,29 +2,33 @@
 const express = require('express');
 const app = express(); 
 const bodyParser = require('body-parser'); 
-const mainRouter = require('./src/routes/mainRoutes'); 
-const authRouter = require('./src/routes/authRoutes');  
+const mainRouter = require('./src/routes/mainRoutes')(); 
+const authRouter = require('./src/routes/authRoutes')();  
 const session = require('express-session');
 const flash= require('connect-flash'); 
 const dotenv = require('dotenv');
+const passport = require('passport'); 
+const cookieParser = require('cookie-parser'); 
 
 dotenv.config();  
 const env = process.env.NODE_ENV; 
 const envString = env.toUpperCase(); 
 const port = process.env['PORT_' + envString]; 
 
-app.set('views', './src/views'); 
-app.set('view engine', 'ejs'); 
 app.use(bodyParser.urlencoded({extended: true})); 
 app.use(bodyParser.json()); 
+app.use(cookieParser()); 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 })); 
+require('./src/config/passport.js')(app); 
 app.use(flash()); 
 app.use('/', mainRouter); 
-app.use('/auth', authRouter); 
+app.use('/auth/', authRouter); 
+app.set('views', './src/views'); 
+app.set('view engine', 'ejs'); 
 
 
 const server = app.listen(port, function(){
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,76 @@
 'use strict'; 
 const express = require('express');
-const router = express.Router(); 
+const { MongoClient } = require('mongodb'); 
+const passport = require('passport'); 
+const { body, validationResult } = require('express-validator/check'); 
+
+const authRouter = express.Router(); 
+
+function router() {
+  authRouter.route('/register')
+    .get((req, res) => {
+      res.render('register', {
+        title: 'Register',
+        errors: []
+      });
+    }) 
+    .post([
+      body('username', 'Empty Username Field').not().isEmpty().trim().escape(), 
+      body('password', 'Password must be at least 5 characters').isLength({ min: 5}),
+      body('password2', 'Password must match').equals(body('password'))
+      ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          const update = errors.array(); 
+          console.log(update); 
+          return res.render('register', {
+            title: 'Register',
+            errors: update 
+          }); 
+        }
+        const { username, password } = req.body; 
+        const url = 'mongodb://localhost:27017'; 
+        const dbName = 'libraryApp'; 
+        (async function addUser() {
+          let client;
+          try {
+            client = await MongoClient.connect(url); 
+            const db = client.db(dbName); 
+            const col = db.collection('users'); 
+            const user = { username, password };
+            const results = await col.insertOne(user);
+            req.login(results.ops[0], () => {
+              res.redirect('/auth/profile');
+            }); 
+          } catch (err) {
+              console.log(err); 
+            }
+          }()); 
+      }); 
+  authRouter.route('/login')
+   .get((req, res) => {
+     res.render('login', {
+       title: 'Login',
+       errors: []
+      }); 
+    }) 
+   .post(passport.authenticate('local', {
+     successRedirect: '/auth/profile',
+     failureRedirect: '/auth/login'
+    }));  
+  authRouter.route('/profile')
+    .all((req, res, next) => {
+      if (req.user) {
+        next(); 
+      } else {
+        res.redirect('/'); 
+      }
+    }) 
+    .get((req, res) => {
+      res.json(req.user); 
+    }); 
+  return authRouter; 
+}
+
 
 module.exports = router; 
